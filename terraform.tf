ef2698777938e4595701ee2441979784a87a4263
Umair Khan <mrumairkhan1@gmail.com>
2020-06-02 16:11:15 +0100

    Updating terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -10,10 +10,7 @@ resource "aws_key_pair" "auth" {
 
 data "aws_ami" "amazon-linux-2" {
  most_recent = true
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
+ owners = ["amazon"]
  filter {
    name   = "name"
    values = ["amzn2-ami-hvm*"]
